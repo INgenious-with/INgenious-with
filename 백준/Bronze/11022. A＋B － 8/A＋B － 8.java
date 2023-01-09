5c718ba61ee6cef4ac411c02680df29b5612bc42
injin KIM <120389213+somizoo@users.noreply.github.com>
2023-01-09 23:23:39 +0900

    [Bronze V] Title: A+B - 8, Time: 272 ms, Memory: 18964 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		
+		Scanner in = new Scanner(System.in);
+
+		int T = in.nextInt();
+		int N = 0;
+		
+		for(int i=0; i<T; i++) {
+			int a = in.nextInt();
+			int b = in.nextInt();
+			int c = a+b;
+			N++;
+			System.out.println("Case #" +N+": "+ a + " + " + b + " = " + c);
+			
+		}
+	}
+}
